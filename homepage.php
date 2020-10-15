9b0c6bcf1feade17114511bb67f9e694587238fe
sdouadi <sdouadi@gmail.com>
2020-10-15 15:11:51 +0200

    MODIF TITRE HOMEPAGE
    J'ai modifié ton titre pour tester ;)

--- homepage.php
+++ homepage.php
@@ -28,9 +28,9 @@
     <head>
         			<meta charset="utf-8" />
       				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      				<title>Mégane projet bibliothèque/title>
+      				<title>Mégane projet bibliothèque TEST GITHUB</title>
       				<link href="../css/mb-comingsoon.min.css" rel="stylesheet" />
-​				       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+				       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
 
           <!--lien pour les carrousels-->
 					<link rel="stylesheet" href="../assets/owl.carousel.min.css"/>
