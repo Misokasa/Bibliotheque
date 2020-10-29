d8d7fdaeaf880194294a272e43e8483eb29f78fc
Misokasa <misokasa@users.noreply.github.com>
2020-10-29 15:31:35 +0100

    Create livreapi.php

---
+++ livrebackend/livreapi.php
@@ -0,0 +1,30 @@
+<?php
+//phpinfo();
+
+    include "../includes/database.php";
+    include "../includes/functions.php";
+
+    $titre = securisation(@$_GET['titre']);
+    $sql = "SELECT * FROM livre WHERE titre LIKE '%".$titre."%'";
+
+    $sth = $dbco->prepare($sql);
+    $sth->execute();
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    $livreList=array();
+    $json="[";
+    $i=0;
+    foreach ($result as $row => $livre) {
+      if($i>0)$json .=",";
+       $json .="{";
+        $livreList[]=$livre;
+        $json .='"id_livre":"'.$livre['id_livre'].'"';
+        $json .=',"titre":"'.$livre['titre'].'"';
+        $json .="}";
+        $i++;
+     }
+
+     $json .="]";
+
+  echo $json;
+
+?>
