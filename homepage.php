335bb89e69a49677120a5d9902548e06b284071b
Misokasa <misokasa@users.noreply.github.com>
2020-10-29 11:44:17 +0100

    Update homepage.php
    Modif nom de page

--- homepage.php
+++ homepage.php
@@ -35,7 +35,7 @@
         			<meta charset="utf-8" />
       				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-      				<title>Mégane projet bibliothèque TEST GITHUB</title>
+      				<title> Projet bibliothèque GITHUB </title>
 
       				<link href="../css/mb-comingsoon.min.css" rel="stylesheet" />
 				       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
