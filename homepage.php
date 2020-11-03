6d4a63f3694d65a3e6c4357a6de0bbfb8a6e8ce4
Misokasa <misokasa@users.noreply.github.com>
2020-11-03 13:21:01 +0100

    Update homepage.php
    Mise en page Code

--- homepage.php
+++ homepage.php
@@ -18,7 +18,7 @@
       				<title> Projet bibliothèque GITHUB  </title>
 
       				<link href="../css/mb-comingsoon.min.css" rel="stylesheet" />
-				       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+				      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
 
           <!--lien pour les carrousels-->
 					<link rel="stylesheet" href="../assets/owl.carousel.min.css"/>
