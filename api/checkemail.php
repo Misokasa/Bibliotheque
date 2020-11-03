44b8f358cf22bc7655791f5dd361d7c2da793485
Misokasa <misokasa@users.noreply.github.com>
2020-11-03 15:22:29 +0100

    Copy checkemail.php & validatepasswod.php
    Création d'un nouveau dossier .. /api dans tpweb/..
    + copie checkemail.php & validatepasswod.php
    
    ../api/checkemail.php = ../ajax/traitement.php

---
+++ api/checkemail.php
@@ -0,0 +1,19 @@
+<?php
+include "database.php";
+
+@$email=$_GET["email"];
+// Instruction : Vérifier si $mail existe dans la table users
+$sql = "SELECT * FROM users WHERE email='$email'";
+
+
+$sth = $dbco->prepare($sql);
+$sth->execute();
+$resultat = $sth->fetch(PDO::FETCH_ASSOC);
+if($sth->rowCount()>0){
+  echo "Nop";
+}
+else {
+  echo "Yeah";
+}
+
+?>
