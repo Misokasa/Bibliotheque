73b2ca5111a269a75e98630ace48068f5775a05a
Misokasa <misokasa@users.noreply.github.com>
2020-11-04 10:54:53 +0100

    Update starter.php
    Ajout <div class="container"> Pour separer Navbar et Siedbar

--- admin/starter.php
+++ admin/starter.php
@@ -27,6 +27,7 @@ include "../security/secure.php";
 
     <body>
 
+     <div class="container">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">La Grande Salle</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,6 +42,7 @@ include "../security/secure.php";
 
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Univers </a>
+
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="#"> Science - Fiction </a>
                 <a class="dropdown-item" href="#"> Polar </a>
@@ -50,14 +52,17 @@ include "../security/secure.php";
               <a class="nav-link" href="#"> A la Une <span class="sr-only">(current)</span></a>
             </li>
 
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Rechercher livre, auteur, ..." aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Rechercher livre, auteur, ..." aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div>
 
 
+    <div clas="container">
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <a href="Adminlte/index3.html" class="brand-link">
           <img src="../img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
@@ -116,6 +121,7 @@ include "../security/secure.php";
           </nav>
         </div>
       </aside>
+    </div>
 
       <div class="container-fluid">
         <?php
